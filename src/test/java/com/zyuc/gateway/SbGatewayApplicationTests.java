5894f3f2b135f8dcfda05dd19fd7fbd3488952b7
hongwj <2233128678@qq.com>
2021-01-08 09:37:49 +0800

    第一次提交

---
+++ src/test/java/com/zyuc/gateway/SbGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyuc.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
